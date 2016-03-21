74ffc23407680dde00f2305d12b560f3d7132a24
weejh <wee_jh@yahoo.com>
2016-03-21 22:27:44 +0800

    to add google map

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,9 +7,9 @@
  * # MainCtrl
  * Controller of the jschallengeApp
  */
-angular.module('jschallengeApp')
+var myCar = angular.module('jschallengeApp');
 
-.controller('MainCtrl', function($scope, $http) {
+myCar.controller('MainCtrl', function($scope, $http) {
 
   // Query for a booking in 1 day from now, for 2 hours.
   var start = Date.now() + 24 * 3600 * 1000;
@@ -20,10 +20,43 @@ angular.module('jschallengeApp')
     console.log('Result from the API call:', results);
     console.log(results[0].description);
     $scope.cars = results;
+    return results;
   }).error(function(err) {
     // Hum, this is odd ... contact us...
     console.error(err);
   });
 
 
+});
+
+myCar.directive('myMaps', function (){
+  return {
+    restrict: 'E',
+    scope: {
+       Lat: '@',
+       Lng: '@'
+    },
+    template: '<div></div>',
+    replace: true,
+    link: function(scope, element, attrs){
+      var myLatLng = new google.maps.LatLng(scope.Lat, scope.Lng);
+
+      var mapOptions = {
+        center: myLatLng,
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      var map = new google.maps.Map(document.getElementById(attrs.id), mapOptions);
+
+      var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Hello World!'
+      });
+
+      marker.setMap(map);
+
+    }
+  };
 });
